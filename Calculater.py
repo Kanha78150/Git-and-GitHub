380abc0a41023fb8b711fc3f1f5f3a8e5b71a871
Bholasankar <bholasankarnanda592@gmail.com>
2023-01-14 19:58:18 +0530

    Initial commit

---
+++ Calculater.py
@@ -0,0 +1,17 @@
+a = int(input("Enter the first number = "))\
+     
+b = int(input("Enter the second number = "))
+
+print("Addition of ", a ," and " , b , " is = " , a+b) 
+
+print("Substraction of ", a ," and " , b , " is = " , a-b) 
+
+print("Multipliction of ", a ," and " , b , " is = " , a*b) 
+
+print("Division of ", a ," and " , b , " is = " , a/b) 
+
+print("FloorDivision of ", a ," and " , b , " is = " , a//b) 
+
+print("Modulo of ", a ," and " , b , " is = " , a%b) 
+
+print("Exponate of ", a ," and " , b , " is = " , a**b) 
