1e1c66f72c8ae57cdb956f414ab39a341bcf8bdb
Bholasankar <bholasankarnanda592@gmail.com>
2023-01-14 20:27:55 +0530

    Add more python

---
+++ Calculater2.py
@@ -0,0 +1,16 @@
+a = int("12")
+
+b = int("12")
+print("Addition of two number = " , a+b)
+
+print("Substraction of two number = " , a-b)
+
+print("Multipliction of two number = " , a*b) 
+
+print("Division of two number = " , a/b) 
+
+print("Floor Division of two number = " , a//b)
+
+print("Exponate of two number = " , a**b)
+
+print("Modulus of two number = " , a%b)
